c215f4f39009d486826412928144361ab3549cdc
AliceColombari <colombari.23@gmail.com>
2022-01-25 10:56:08 +0100

    esercizio corretto

--- main.js
+++ main.js
@@ -5,17 +5,23 @@ generateButton.addEventListener("click",
     function() {
 
         // INPUT DA INSERIRE
-        let name = document.getElementById('name').value;
-        let km = document.getElementById('km').value;
-        let age = document.getElementById('age').value;
+        const name = document.getElementById('name').value;
+        console.log(name);
+
+        const km = document.getElementById('km').value;
+        console.log(km);
+
+        const age = document.getElementById('age').value;
+        console.log(age);
 
         // calcolo il prezzo del biglietto
         // prezzo per km
         let pricePerTicket = 0.21;
-        console.log(pricePerTicket);
+        console.log("Il prezzo per Km è pari a: " + pricePerTicket);
 
         // Calcolo prezzo biglietto di listino - senza sconto
         const priceTicket = km * pricePerTicket;
+        console.log("Il prezzo del biglietto senza sconto è pari a: " + priceTicket);
 
         // Sconto under 18
         let sale20 = priceTicket - (priceTicket * 0.2);
@@ -23,39 +29,48 @@ generateButton.addEventListener("click",
         let sale40 = priceTicket - (priceTicket * 0.4);
 
         let typeUser = 'Biglietto Standard';
-        var finalPrice; 
+        let finalPrice; 
 
         // Condizione sconto applicato in base all'età
         if (age == 'under18') {
             finalPrice = sale20;
             typeUser = 'Sconto Junior';
+            console.log("Sconto junior: " + finalPrice)
 
         } else if (age == 'over65') {
             finalPrice = sale40;
             typeUser = 'Sconto Senior';
+            console.log("Sconto Senior: " + finalPrice)
 
         } else {
             finalPrice = priceTicket;
-            console.log(finalPrice);
+            console.log("Sconto Adult: " + finalPrice)
         }
 
         // generatore da 1 a 9 per cabina
-        var carriageNumber = Math.floor((Math.random() * 9) + 1);
+        let carriageNumber = Math.floor((Math.random() * 9) + 1);
 
-        // generatore min e max per CP
-        var min = 90000;
-        var max = 99999;
-        var discountCode = Math.floor(Math.random() * (max - min) ) + min;
+        // generatore numero compreso tra 90000 e 99999 per codice CP
+        let discountCode = Math.floor(Math.random() * 10000) + 90000;
 
         // STAMPA DATI SU TICKET DETAIL
+
+        // trascrivo nome passeggero nel biglietto
         document.getElementById('passanger_name').innerHTML = name;
 
+        // riporto il nome dell'offerta
         document.getElementById('discount_name').innerHTML = typeUser;
+
+        // genero un numero random della carrozza
         document.getElementById('carriage').innerHTML = carriageNumber;
+
+        // genero un numero random del codice CP
         document.getElementById('discount_code').innerHTML = discountCode;
+
+        // riporto il prezzo del biglietto
         document.getElementById('final_price').innerHTML = finalPrice.toFixed(2) + ' €';
 
-        // Funzione per mostrare i dettagli del biglietto al click del pulsante
+        // Funzione per mostrare i dettagli del biglietto al click del pulsante - richiamo classe style
         document.getElementById('detail_container_passanger').className = 'show';
     }
 );
@@ -66,7 +81,13 @@ var eraseButton = document.getElementById("reset");
 
 eraseButton.addEventListener("click", 
     function() {
-        // Funzione per nascondere i dettagli del biglietto al click del pulsante
+        
+        // reset valori cliccando su annulla
+        document.getElementById('name').value = "";
+        document.getElementById('km').value = "";
+        document.getElementById('age').value = "";
+        
+        // Funzione per nascondere i dettagli del biglietto al click del pulsante - richiamo classe style
         document.getElementById('detail_container_passanger').className = 'hidden';
     }
 );
